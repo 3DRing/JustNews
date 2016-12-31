e679249ce79aa4335a24f0e185645afe58ad842b
Ringov <by3dring@gmail.com>
2016-12-31 18:21:58 +0500

    A few fixes

--- app/src/main/java/com/ringov/justnews/view/WidgetFragment.java
+++ app/src/main/java/com/ringov/justnews/view/WidgetFragment.java
@@ -18,6 +18,6 @@ public class WidgetFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.widget_layout_light, container, false);
+        return inflater.inflate(R.layout.widget_layout_dark, container, false);
     }
 }
